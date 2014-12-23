6972497e41abf889a9a5c39071abc4a1577139f6
Andrey Sekretenko <numpor@yandex-team.ru>
2014-12-23 20:03:09 +0300

    Protocol description and test stage order

---
+++ yandex-tank-api/common.py
@@ -0,0 +1,42 @@
+"""
+Commands from HTTP Server (from manager_queue):
+    Run the test to the next break:
+        {
+        'session': '1a2b4f3c'
+        'cmd':'run',
+        'break': --- see break requests for tank
+        'test': --- only when creating new session
+        'config': --- only when creating new  session
+        }
+    Stop the test
+        {
+        'session': '1a2b4f3c'
+        'cmd':'stop'
+        }
+            
+
+Status reported by tank (from manager_queue):
+    {
+     'status':'running'|'success'|'failed',
+     'current_stage': --- the same as in break request, optional
+     'break': --- stage to make a break before
+     'reason': --- optional 
+     }    
+=====
+
+Break requests (into tank_queue):
+    {'break':'lock'|'configure'|'prepare'|'start'|'poll'|'end'|'postprocess'|'finish'|'none'}
+
+====
+Status reported to HTTP Server (into webserver_queue):
+    {
+    'session': '1a2b4f3c'
+    'test': 'DEATHSTAR-10-12345'
+    'status': 'running'|'success'|'failed' (from tank or from  manager)
+    'stage': --- optional, from tank only
+    'reason' : --- optional (from tank or from manager)
+    }
+"""
+
+test_stage_order=['lock','configure','prepare','start','poll','end','postprocess','finish','none']
+
