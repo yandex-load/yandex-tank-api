8564ad3743a952c9e40591bebe27caae3e34ef39
Andrey Sekretenko <numpor@yandex-team.ru>
2014-12-24 13:56:04 +0300

    Init Webserver

--- yandex-tank-api/webserver.py
+++ yandex-tank-api/webserver.py
@@ -4,5 +4,14 @@ import tornado
 import common
 
 def run(webserver_queue, manager_queue):
-    """Target for webserver process"""
-    raise NotImplementedError("webserver not implemented")
+    """Target for webserver process.
+    The only function ever used by the Manager.
+
+    webserver_queue
+        Read statuses from Manager here.
+
+    manager_queue
+        Write commands for Manager there.
+    
+    """
+    raise NotImplementedError("Webserver not implemented")
