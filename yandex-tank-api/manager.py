eefe91c64348d9880b50a9dc58a0f2b3d204dbde
Andrey Sekretenko <numpor@yandex-team.ru>
2014-12-24 14:00:24 +0300

    Started work on Manager

---
+++ yandex-tank-api/manager.py
@@ -0,0 +1,24 @@
+import common
+
+class TankRunner:
+    """
+    Manages the tank process
+    """
+
+
+class Manager:
+    """
+    Implements message processing logic
+    """
+    def __init__(self,manager_queue,webserver_queue):
+        self.manager_queue=manager_queue
+        self.webserver_queue=webserver_queue
+
+def run_server():
+    """
+    Daemonize
+    Create queues for manager and webserver
+    Fork webserver
+    Run
+    """
+
