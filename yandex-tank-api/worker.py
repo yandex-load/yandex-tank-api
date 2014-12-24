0b45ee460737c4a30745d761cbeb761ebe85db38
Andrey Sekretenko <numpor@yandex-team.ru>
2014-12-24 18:05:08 +0300

    Added work_dir to run() parameters

--- yandex-tank-api/worker.py
+++ yandex-tank-api/worker.py
@@ -2,7 +2,7 @@
 #Test stage order, internal protocol description, etc...
 import common
 
-def run(tank_queue,manager_queue):
+def run(tank_queue,manager_queue,work_dir):
     """
     Target for tank process.
     This is the only function from this module ever used by Manager.
