16b3224204fc1d14b62992ca4cb1d11262834df8
Andrey Sekretenko <numpor@yandex-team.ru>
2014-12-24 14:00:01 +0300

    Init Worker

---
+++ yandex-tank-api/worker.py
@@ -0,0 +1,17 @@
+
+#Test stage order, internal protocol description, etc...
+import common
+
+def run(tank_queue,manager_queue):
+    """
+    Target for tank process.
+    This is the only function from this module ever used by Manager.
+
+    tank_queue
+        Read next break from here
+
+    manager_queue
+        Write tank status there
+       
+    """
+    pass
