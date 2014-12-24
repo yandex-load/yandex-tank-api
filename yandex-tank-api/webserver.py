57ad333542760be1c0c64297205a7844c5cc1385
Andrey Sekretenko <numpor@yandex-team.ru>
2014-12-24 13:55:41 +0300

    Init Webserver

---
+++ yandex-tank-api/webserver.py
@@ -0,0 +1,8 @@
+import tornado
+
+#Test stage order, internal protocol description, etc...
+import common
+
+def run(webserver_queue, manager_queue):
+    """Target for webserver process"""
+    raise NotImplementedError("webserver not implemented")
