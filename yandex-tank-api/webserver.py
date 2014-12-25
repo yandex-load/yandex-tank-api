ec49b69a585a195eb89ebf097942c084525ef0d0
Andrey Sekretenko <numpor@yandex-team.ru>
2014-12-25 18:40:24 +0300

    Implemented /run handler; run through pylint

--- yandex-tank-api/webserver.py
+++ yandex-tank-api/webserver.py
@@ -6,6 +6,7 @@ import tornado.web
 import os.path
 import json
 import uuid
+import multiprocessing
 
 import common
 
@@ -14,55 +15,113 @@ from pyjade.ext.tornado import patch_tornado
 patch_tornado()
 
 class RunHandler(tornado.web.RequestHandler):
-    def initialize(self, out_queue, sessions):
+    def initialize(self, out_queue, sessions, working_dir):
+        """
+        sessions
+            dict: session_id->session_status
+        """    
         self.out_queue = out_queue
         self.sessions = sessions
+        self.working_dir = working_dir
 
     def post(self):
 
         test_id = self.request.arguments.get("test", uuid.uuid4().hex)
-        breakpoint = self.request.arguments.get("break", "none")
+        breakpoint = self.request.arguments.get("break", "finish")
         session_id = uuid.uuid4().hex
         config = self.request.body
 
-        # TODO: test for existing test with this id
-        # TODO: answer 409 if exists finished test
-        # TODO: answer 503 if exists and running
-        # TODO: answer 503 if other test is running
+        self.set_header("Content-type", "application/json")
 
-        # TODO: post run command to manager queue
-        print config
+        #Check existing sessions
+        running_session=None
+        conflict_session=None
+        for s in self.sessions:
+            if s['status'] not in ['success','finished']:
+                running_session=s
+            if s['test']==test_id:
+                conflict_session=s
+
+        # 409 if session with this test_id is already running
+        if conflict_session is not None:
+                self.set_status(409)
+                self.finish(json.dumps(conflict_session))
+                return
+
+        # 409 if finished test with this test_id exists
+        test_status_file=self.working_dir+os.sep+'status.json'
+        if os.path.exists(test_status_file):
+            self.set_status(409)
+            self.finish(open(test_status_file).read())
+            return
+
+
+        # 503 if any running session exists (but no test_id conflict)
+        if running_session is not None:
+                self.set_status(503)
+                self.finish(json.dumps(running_session))
+                return
+
+        #Remember that such session exists
+        self.sessions[session_id]={'status':'starting',
+                              'break':breakpoint,
+                              'test': test_id}
+
+        # Post run command to manager queue
+        self.out_queue.put({'session':session_id,
+                       'cmd':'run',
+                       'break':breakpoint,
+                       'test':test_id,
+                       'config':config})
 
         self.set_status(200)
-        self.set_header("Content-type", "application/json")
-        self.finish(json.dumps(
-            {
-                "test": test_id,
-                "session": session_id,
-                "breakpoint": breakpoint,
-            }
-        ))
+        self.finish(json.dumps({
+                               "test": test_id,
+                               "session": session_id,
+                               }))
 
     def get(self):
-        breakpoint = self.request.arguments.get("break", "none")
+        breakpoint = self.request.arguments.get("break", "finish")
         session_id = self.request.arguments.get("session")
+        self.set_header("Content-type", "application/json")
 
-        # TODO: find session in db
-        # TODO: 404 if no such session
-        # TODO: 418 if in higher state
-        # TODO: 500 if failed
-        # TODO: post run command to manager queue
+        # 400 if invalid breakpoint
+        if not breakpoint in common.test_stage_order:
+            self.set_status(400)
+            self.finish(json.dumps({'reason':'Invalid break point',
+                                    'hint':
+                                        {'breakpoints':common.test_stage_order}
+                                   }))
+            return
+
+        # 404 if no such session
+        if not session_id in self.sessions:
+            self.set_status(404)
+            self.finish(json.dumps({'reason':'No session with this ID'}))
+            return
+        status_dict=self.sessions[session_id]
+
+        # 500 if failed
+        if status_dict['status'] == 'failed':
+            self.set_status(500)
+            self.finish(json.dumps(status_dict))
+            return
+
+        # 418 if in higher state or not running
+        if status_dict['status'] == 'success' or common.is_A_earlier_than_B(breakpoint,status_dict['break']):
+            reply={'reason':'I am a teapot! I know nothing of time-travel!',
+                    'hint': {'breakpoints':common.test_stage_order} }
+            reply.update(status_dict)
+            self.set_status(418)
+            self.finish(json.dumps(reply))
+            return
+
+        # Post run command to manager queue
+        self.out_queue.put({'session':session_id,
+                       'cmd':'run',
+                       'break':breakpoint})
 
         self.set_status(200)
-        self.set_header("Content-type", "application/json")
-        self.finish(json.dumps(
-            {
-                # "test": test_id,
-                "session": session_id,
-                "breakpoint": breakpoint,
-            }
-        ))
-
 
 class StopHandler(tornado.web.RequestHandler):
     def initialize(self, out_queue, sessions):
@@ -70,11 +129,9 @@ class StopHandler(tornado.web.RequestHandler):
         self.sessions = sessions
 
     def get(self):
-        breakpoint = self.request.arguments.get("break", "none")
         session_id = self.request.arguments.get("session")
 
-        # TODO: find session in db
-        # TODO: 404 if no such session
+        # 404 if no such session
         # TODO: post stop command to manager queue
 
         self.set_status(200)
@@ -126,13 +183,7 @@ class ArtifactHandler(tornado.web.RequestHandler):
 
         self.set_status(200)
         self.set_header("Content-type", "application/json")
-        self.finish(json.dumps(
-            {
-                # "test": test_id,
-                "session": session_id,
-                "status": "",
-            }
-        ))
+        self.finish(json.dumps(dict()))
 
 
 class ApiServer(object):
@@ -154,13 +205,20 @@ class ApiServer(object):
         )
 
     def update_status(self):
-        while message = self.in_queue.get_nowait():
-            self.sessions[message.get('session')] = message
+        try:
+           while True:
+               message = self.in_queue.get_nowait()
+               session_id=message.get('session')
+               del message['session']
+               #Test ID and break are always present in message from Manager
+               self.sessions[session_id] = message
+        except multiprocessing.queues.Empty:
+            pass
 
     def serve(self):
         self.app.listen(8888)
         ioloop = tornado.ioloop.IOLoop.instance()
-        update_cb = tornado.ioloop.PeriodicCallback(self.update_status, 500, ioloop)
+        update_cb = tornado.ioloop.PeriodicCallback(self.update_status, 100, ioloop)
         update_cb.start()
         ioloop.start()
 
