e028498feadb54820ee97d0e655bd28eac982f57
Alexey Lavrenuke <direvius@gmail.com>
2014-12-25 19:40:18 +0300

    Artifact handler

--- yandex-tank-api/webserver.py
+++ yandex-tank-api/webserver.py
@@ -4,6 +4,7 @@ import logging
 import tornado.ioloop
 import tornado.web
 import os.path
+import os
 import json
 import uuid
 import multiprocessing
@@ -14,12 +15,16 @@ from tornado import template
 from pyjade.ext.tornado import patch_tornado
 patch_tornado()
 
+
+# TODO: make it configurable
+TRANSFER_SIZE_LIMIT = 128 * 1024
+
 class RunHandler(tornado.web.RequestHandler):
     def initialize(self, out_queue, sessions, working_dir):
         """
         sessions
             dict: session_id->session_status
-        """    
+        """
         self.out_queue = out_queue
         self.sessions = sessions
         self.working_dir = working_dir
@@ -49,7 +54,7 @@ class RunHandler(tornado.web.RequestHandler):
                 return
 
         # 409 if finished test with this test_id exists
-        test_status_file=self.working_dir+os.sep+'status.json'
+        test_status_file=os.path.join(self.working_dir, test_id, 'status.json')
         if os.path.exists(test_status_file):
             self.set_status(409)
             self.finish(open(test_status_file).read())
@@ -124,7 +129,7 @@ class RunHandler(tornado.web.RequestHandler):
         self.set_status(200)
 
 class StopHandler(tornado.web.RequestHandler):
-    def initialize(self, out_queue, sessions):
+    def initialize(self, out_queue, sessions, working_dir):
         self.out_queue = out_queue
         self.sessions = sessions
 
@@ -144,7 +149,7 @@ class StopHandler(tornado.web.RequestHandler):
         ))
 
 class StatusHandler(tornado.web.RequestHandler):
-    def initialize(self, out_queue, sessions):
+    def initialize(self, out_queue, sessions, working_dir):
         self.out_queue = out_queue
         self.sessions = sessions
 
@@ -168,36 +173,86 @@ class StatusHandler(tornado.web.RequestHandler):
 
 
 class ArtifactHandler(tornado.web.RequestHandler):
-    def initialize(self, out_queue, sessions):
+    def initialize(self, out_queue, sessions, working_dir):
         self.out_queue = out_queue
         self.sessions = sessions
+        self.working_dir = working_dir
 
     def get(self):
         test_id = self.request.arguments.get("test")
         filename = self.request.arguments.get("filename")
 
-        # TODO: return list of filest for this test if no filename specified
-        # TODO: return file if found
-        # TODO: 404 if no such file
-        # TODO: 503 if too large file and shooting in progress
 
-        self.set_status(200)
-        self.set_header("Content-type", "application/json")
-        self.finish(json.dumps(dict()))
+        # look for status.json (any test should have it)
+        if not os.path.exists(os.path.join(self.working_dir, test_id, 'status.json')):
+            self.set_header("Content-type", "application/json")
+            self.set_status(404)
+            self.finish(json.dumps({
+                'reason': 'No test with this ID found',
+                'test': test_id,
+            }))
+            return
+
+        if filename:
+            filepath = os.path.join(self.working_dir, test_id, filename)
+            if os.path.exists(filepath):
+                file_size = os.stat(filepath)
+
+                if file_size > TRANSFER_SIZE_LIMIT and any(s['status'] not in ['success','finished'] for s in self.sessions):
+                    self.set_header("Content-type", "application/json")
+                    self.set_status(503)
+                    self.finish(json.dumps({
+                        'reason': 'File is too large and test is running',
+                        'test': test_id,
+                        'filename': filename,
+                        'filesize': file_size,
+                        'limit': TRANSFER_SIZE_LIMIT,
+                    }))
+                    return
+                else:
+                    self.set_header("Content-type", "application/octet-stream")
+                    with open(filepath, 'rb') as f:
+                        data = f.read()
+                        self.write(data)
+                    self.finish()
+                    return
+            else:
+                self.set_header("Content-type", "application/json")
+                self.set_status(404)
+                self.finish(json.dumps({
+                    'reason': 'No such file',
+                    'test': test_id,
+                    'filename': filename,
+                }))
+                return
+        else:
+            basepath = os.path.join(self.working_dir, test_id)
+            onlyfiles = [
+                f for f in os.listdir(basepath)
+                if os.path.isfile(os.path.join(basepath, f))
+            ]
+            self.set_header("Content-type", "application/json")
+            self.set_status(200)
+            self.finish(json.dumps(onlyfiles))
 
 
 class ApiServer(object):
     def __init__(self):
         self.in_queue = None # TODO: pass it as a parameter
         self.out_queue = None # TODO: pass it as a parameter
+        self.working_dir = None # TODO: pass it as a parameter
         self.sessions = {}
-        handler_params = dict(out_queue=self.out_queue, sessions=self.sessions)
+        handler_params = dict(
+            out_queue=self.out_queue,
+            sessions=self.sessions,
+            working_dir=self.working_dir,
+        )
         self.app = tornado.web.Application(
             [
                 (r"/run", RunHandler, handler_params),
-                #   (r"/stop", StopHandler),
-                #   (r"/status", StatusHandler),
-                #   (r"/artifact", ArtifactHandler),
+                (r"/stop", StopHandler, handler_params),
+                (r"/status", StatusHandler, handler_params),
+                (r"/artifact", ArtifactHandler, handler_params),
             ],
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
             static_path=os.path.join(os.path.dirname(__file__), "static"),
