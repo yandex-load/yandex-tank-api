10eb15b1afcf6179a64a3d14e4cedcf98221e139
danila <danila@yandex-team.ru>
2015-01-14 20:07:09 +0300

    Example of multishoot + phout aggregator

---
+++ examples/multishoot/phout_aggregator.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import sys
+import argparse
+
+BUFFER_BYTES_IN  = 300000
+BUFFER_BYTES_OUT = 1000000
+
+def phout_reader(filename):
+    f = open(filename, 'r', BUFFER_BYTES_IN)
+    while True:
+        line = f.readline()
+        yield(line, calculate_ts(line))
+
+def calculate_ts(line):
+    a = line.split("\t", 3)
+    return float(a[0]) - float(a[2])/1000
+
+def merge_phouts(filenames=[], outfilename='result_phout.txt'):
+    outfile = open(outfilename, 'w', BUFFER_BYTES_OUT)
+    readers = []
+    current = []
+    left_files = []
+
+    for i in range(len(filenames)):
+        reader = phout_reader(filenames[i])
+        readers.append(reader)
+        try:
+            current.append(next(reader))
+            left_files.append(i)
+        except:
+            pass
+
+    while len(left_files):
+        i = min(left_files, key=lambda x: current[x][1])
+        outfile.write(current[i][0])
+        try:
+            current[i] = next(readers[i])
+        except:
+            left_files.remove(i)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='-i=FILE1.txt,FILE2.txt,FILE3.txt -o RESULT.txt')
+    parser.add_argument('-i', action='store', dest='in_files', type=str)
+    parser.add_argument('-o', action='store', dest='outfilename', type=str)
+
+    args = parser.parse_args()
+    in_files = args.in_files.split(",")
+    merge_phouts(in_files, args.outfilename)
